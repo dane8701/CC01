3886eccc0732b537c0d3a0b6cae2e8ecb36120d7
BorelPayong <68334292+BorelPayong@users.noreply.github.com>
2020-11-19 04:13:27 +0100

    Creation de la BO

--- CC01.BO/Student.cs
+++ CC01.BO/Student.cs
@@ -8,5 +8,47 @@ namespace CC01.BO
 {
     public class Student
     {
+        public string University;
+
+        public string Firstname;
+
+        public string Lastname;
+
+        public string Born;
+
+        public string At;
+
+        public string Contact;
+
+        public byte[] picture;
+
+        public string Reference;
+
+        public Student()//serialisation
+        {
+        }
+
+        public Student(string university, string firstname, string lastname, string born, string at, string contact, byte[] picture, string reference)
+        {
+            University = university;
+            Firstname = firstname;
+            Lastname = lastname;
+            Born = born;
+            At = at;
+            Contact = contact;
+            this.picture = picture;
+            Reference = reference;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   Reference == student.Reference;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1304721846 + EqualityComparer<string>.Default.GetHashCode(Reference);
+        }
     }
 }
--- CC01.BO/University.cs
+++ CC01.BO/University.cs
@@ -8,5 +8,37 @@ namespace CC01.BO
 {
     public class University
     {
+        public string Nomuniversity;
+
+        public string Born;
+
+        public string Lieu;
+
+        public string Contact;
+
+        public byte[] Logo;
+
+        public string Reference;
+
+        public University(string nomuniversity, string born, string lieu, string contact, byte[] logo, string reference)
+        {
+            Nomuniversity = nomuniversity;
+            Born = born;
+            Lieu = lieu;
+            Contact = contact;
+            Logo = logo;
+            Reference = reference;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is University university &&
+                   Reference == university.Reference;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1304721846 + EqualityComparer<string>.Default.GetHashCode(Reference);
+        }
     }
 }
